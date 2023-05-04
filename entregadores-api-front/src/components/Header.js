1a26f5364706d950084c02fc10f26dda7d114324
Pedro Antônio Silva <105285216+P-ASilva@users.noreply.github.com>
2023-05-04 12:43:55 -0300

    Header sarted

--- entregadores-api-front/src/components/Header.js
+++ entregadores-api-front/src/components/Header.js
@@ -1,9 +1,19 @@
+import Switch from '@mui/material/Switch'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormGroup from '@mui/material/FormGroup'
 
 function Header() {
+  var name = localStorage.getItem('name')
     return (
-      <div className="App">
-        <h1>Hello World</h1>    
+      <div className="Header">
+        <h1>Hello World, {name}</h1> 
+        <div className='SwitchMui'>
+          <FormGroup>
+            <FormControlLabel control={<Switch defaultChecked />} label="Pronto para entrega!" />
+          </FormGroup>
+        </div>
       </div>
+      
     );
   }
   
--- entregadores-api-front/src/pages/Editar.js
+++ entregadores-api-front/src/pages/Editar.js
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import { hasFormSubmit } from "@testing-library/user-event/dist/utils"
 import React, {useState,useEffect} from "react"
 
+
 function Editar() {
 
     const [name,setName] = useState()
@@ -16,30 +17,22 @@ function Editar() {
 
     const handleClick = click  => {
         alert(name + ' ' + cpf + ' ' + veiculo + ' ' )
-        fetch('http://localhost:3000/editar', { 
+        fetch('http://localhost:4000/editar', { 
             method:'POST',
             headers:{"Content-Type":'application/json'},
             body: JSON.stringify(data)}
         ).then(response => {
             if (response.status===200) {
-                alert('Editar realizado com sucesso')
+                alert('Edição realizada com sucesso')
             }
         }).catch(ex => {
-            alert('Erro ao se cadastrar como entregador')
+            alert('Erro ao editar perfil')
         })
     }
 
     return (
     <div>
       <Header />
-        <div>
-            Name: <input id='name' type='text' onChange={e => setName(e.target.value)}/>
-            <br></br>
-            CPF: <input id= 'cpf' type='text' onChange={e => setCpf(e.target.value)}/>
-            <br></br>
-            Veiculo: <input id= 'veiculo' type='text' onChange={e => setVeiculo(e.target.value)}/>
-            <input type='button' onClick={handleClick} value="Enviar" ></input>
-        </div> 
     </div>
     );
   }
