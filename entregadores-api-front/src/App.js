ef0df0390960f80e7a8859050c392c0cf5f4e0ee
Pedro Antônio Silva <105285216+P-ASilva@users.noreply.github.com>
2023-05-04 10:59:55 -0300

    cadastro done

--- entregadores-api-front/src/App.js
+++ entregadores-api-front/src/App.js
@@ -1,6 +1,6 @@
-
 import './App.css';
 import Cadastro from './pages/Cadastro';
+import Test from './pages/Test';
 import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/cadastro' element={<Cadastro/>}></Route>
+        <Route path='/test' element={<Test/>}></Route>
       </Routes>
     </div>
   );
--- entregadores-api-front/src/pages/Cadastro.js
+++ entregadores-api-front/src/pages/Cadastro.js
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
 import { hasFormSubmit } from "@testing-library/user-event/dist/utils"
-import React, {useState,useEffect} from "react"
+import React, {useState,useEffect, Component} from "react"
+import { useNavigate } from "react-router-dom";
+import SignUp from "../components/SignUp";
 
 function Cadastro() {
-
+    const navigate = useNavigate()
     const [name,setName] = useState()
     const [cpf,setCpf] = useState()
     const [veiculo,setVeiculo] = useState()
@@ -16,13 +18,14 @@ function Cadastro() {
 
     const handleClick = click  => {
         alert(name + ' ' + cpf + ' ' + veiculo + ' ' )
-        fetch('http://localhost:3000/cadastro', { 
+        fetch('http://localhost:4000/cadastro', { 
             method:'POST',
             headers:{"Content-Type":'application/json'},
             body: JSON.stringify(data)}
         ).then(response => {
             if (response.status===200) {
                 alert('Cadastro realizado com sucesso')
+                navigate('/test')
             }
         }).catch(ex => {
             alert('Erro ao se cadastrar como entregador')
@@ -32,14 +35,15 @@ function Cadastro() {
     return (
     <div>
       <Header />
-        <div>
+        {/* <div className = 'form'>
             Name: <input id='name' type='text' onChange={e => setName(e.target.value)}/>
             <br></br>
             CPF: <input id= 'cpf' type='text' onChange={e => setCpf(e.target.value)}/>
             <br></br>
             Veiculo: <input id= 'veiculo' type='text' onChange={e => setVeiculo(e.target.value)}/>
             <input type='button' onClick={handleClick} value="Enviar" ></input>
-        </div> 
+        </div>  */}
+        <SignUp></SignUp>
     </div>
     );
   }
