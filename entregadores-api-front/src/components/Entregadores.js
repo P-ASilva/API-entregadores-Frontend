7bfd724a740ef7db1c065584dcc62e2c80b7792c
Pedro Antônio Silva <105285216+P-ASilva@users.noreply.github.com>
2023-05-08 19:39:21 -0300

    bugfix

---
+++ entregadores-api-front/src/components/Entregadores.js
@@ -0,0 +1,72 @@
+import React, {useState,useEffect} from "react"
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+
+function Entregadores() {
+
+  const [entregadores,setEntregadores] = useState([])
+
+  useEffect(()=> {
+      const fetchData = async ()  => {
+          await fetch('http://localhost:8080/entregador', { 
+              method:'GET',
+              headers:{"Content-Type":'application/json'},
+          }
+          ).then(response => {
+              if (response.status === 200 || response.status === 201) {
+                  return response.json()
+              }else {return response.json()}
+          }).then(entregadores => {setEntregadores(entregadores) 
+              setEntregadores(entregadores)
+              alert(JSON.stringify(entregadores))
+          }
+          ).catch(ex => {
+              alert(ex)
+          })
+        }
+      fetchData()
+  },[])
+
+
+
+  return (
+      <div>
+          <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+              {entregadores.map(entregador => {
+                  return(
+                      <div key={entregador.id}>
+                          <ListItem alignItems="flex-start">
+                              <ListItemAvatar>
+                                  <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                              </ListItemAvatar>
+                              <ListItemText
+                                  primary={entregador.nome}
+                                  secondary={
+                                      <React.Fragment>
+                                      <Typography
+                                          sx={{ display: 'inline' }}
+                                          component="span"
+                                          variant="body2"
+                                          color="text.primary"
+                                      >
+                                          {entregador.tipo_veiculo}
+                                      </Typography>
+                                      {' ' + (entregador.status_ocupacao)}
+                                      </React.Fragment>
+                                  }
+                              />
+                          </ListItem>
+                          <Divider variant="inset" component="li" />
+                      </div>
+                  )
+              })}
+          </List>
+      </div>
+  );
+}
+export default Entregadores
--- entregadores-api-front/src/pages/Listagem.js
+++ entregadores-api-front/src/pages/Listagem.js
@@ -1,14 +1,15 @@
 import Header from "../components/Header";
-import { hasFormSubmit } from "@testing-library/user-event/dist/utils"
+import Entregadores from "../components/Entregadores";
 import React, {useState,useEffect} from "react"
-import axios from 'axios'
+
+
 function Listagem() {
 
     const [entregadores,setEntregadores] = useState([])
 
     useEffect(()=> {
         const fetchData = async ()  => {
-            await fetch('http://localhost:8080/entregadores', { 
+            await fetch('http://localhost:8080/entregador', { 
                 method:'GET',
                 headers:{"Content-Type":'application/json'},
             }
@@ -17,8 +18,9 @@ function Listagem() {
                     return response.json()
                 }else {return response.json()}
             }).then(entregadores => {setEntregadores(entregadores) 
-                alert(entregadores)}
-
+                setEntregadores(entregadores)
+                alert(JSON.stringify(entregadores))
+            }
             ).catch(ex => {
                 alert(ex)
             })
@@ -31,19 +33,9 @@ function Listagem() {
     return (
         <div>
             <Header></Header>
-            <div>
-                {entregadores.map(entregador => {
-                    return(
-                        <div key={entregador.id}>
-                            <h2>{entregador.title}</h2>
-                            <p>{entregador.dates}</p>
-                        </div>
-                    )
-                })}
-            </div>
+            <Entregadores></Entregadores>
         </div>
     );
-  }
+}
   
 export default Listagem;
-  
